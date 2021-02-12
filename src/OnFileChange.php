9234d48d4fd4156ea1e22241a1d09ad2cff77132
Bruno Natali <b010010010n@gmail.com>
2021-02-12 14:32:41 -0300

    Corrected namespace declaration

--- src/OnFileChange.php
+++ src/OnFileChange.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace BrunoNatali\Tools\File;
+namespace BrunoNatali\File;
 
 use BrunoNatali\Inotify\Factory as Inotify;
 use BrunoNatali\Tools\OutSystem;
--- src/OnFileChangeInterface.php
+++ src/OnFileChangeInterface.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace BrunoNatali\Tools\File;
+namespace BrunoNatali\File;
 
 interface OnFileChangeInterface
 {
@@ -54,5 +54,5 @@ interface OnFileChangeInterface
      * @param float $time New polling time
      * @return bool
     */
-    public function setPollingTime(int $time): bool;
+    public function setPollingTime(float $time): bool;
 }
